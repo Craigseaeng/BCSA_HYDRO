4816008a2cee828d600d74b2887f776c8b937768
Chris Flanary <cflanary@users.noreply.github.com>
2015-01-08 11:36:12 -0800

    Updated mhs_out.f90 to include more space for time variable and additional flux calculations

--- mhs_out.f90
+++ mhs_out.f90
@@ -41,9 +41,9 @@ IF(.NOT.FIRSTTIME)THEN
     OPEN (UNIT=107, FILE='shear_cal.dat', STATUS='REPLACE')
 
     ! Initialize variable for first time step
-    vel_maxc=0.0
-    vel_max=0.0
-    tau_max=0.0
+    !vel_maxc=0.0
+    !vel_max=0.0
+    !tau_max=0.0
 
     FIRSTTIME=.TRUE.
 ENDIF
@@ -53,36 +53,51 @@ time_efdc=DT*FLOAT(N)+TCON*TBEGIN
 time_efdc=time_efdc/86400.
 
 IF (ISTRAN(6).EQ.1) THEN
-! Calculate TSS flux at MHS01, MHS06, and MHS07
-DO LOC=1,4
+! Calculate TSS flux at MHS01, MHS02, MHS05, MHS06, MHS07, and the LBC-BCC connector
+DO LOC=1,6
+    ! Initialize temporary variables
     flow_u(LOC)=0.0
     flow_v(LOC)=0.0
     tss_flux_u(LOC)=0.0
     tss_flux_v(LOC)=0.0
 
     SELECT CASE (LOC)
-    CASE(1)
-    ITEMP1=116;ITEMP2=129;JTEMP1=114;JTEMP2=114
+	    CASE(1)
+		    ! MHS01 flux line
+            ITEMP1=116;ITEMP2=129;JTEMP1=114;JTEMP2=114
 
-    CASE(2)
-    ITEMP1=35;ITEMP2=41;JTEMP1=203;JTEMP2=203
+        CASE(2)
+		    ! MHS02 flux line
+            ITEMP1=43;ITEMP2=46;JTEMP1=23;JTEMP2=23
 
-    CASE(3)
-    ITEMP1=119;ITEMP2=119;JTEMP1=311;JTEMP2=314
+        CASE(3)
+		    ! MHS05 flux line
+            ITEMP1=35;ITEMP2=41;JTEMP1=203;JTEMP2=203
 
-    CASE(4)
-    ITEMP1=128;ITEMP2=131;JTEMP1=349;JTEMP2=349
+        CASE(4)
+		    ! MHS06 flux line
+            ITEMP1=119;ITEMP2=119;JTEMP1=311;JTEMP2=314
+			
+		CASE(5)
+		    ! MHS07 flux line
+		    ITEMP1=128;ITEMP2=131;JTEMP1=349;JTEMP2=349
+			
+		CASE(6)
+		    ! LBC-BCC connector flux line
+		    ITEMP1=40;ITEMP2=40;JTEMP1=179;JTEMP2=179
     END SELECT
 
     DO I=ITEMP1,ITEMP2
         DO J=JTEMP1,JTEMP2
             IF(LMASKDRY(LIJ(I,J))) THEN
+			! Compute flow rate
             flow_u_tmp=U(LIJ(I,J),1)*HP(LIJ(I,J))*DXU(LIJ(I,J))
             flow_u(LOC)=flow_u(LOC) + flow_u_tmp
             flow_v_tmp=V(LIJ(I,J),1)*HP(LIJ(I,J))*DYV(LIJ(I,J))
             flow_v(LOC)=flow_v(LOC) + flow_v_tmp
             
                 DO K=1,NSCM
+				    ! Compute TSS flux for all size classes
                     tss_flux_u_tmp=U(LIJ(I,J),1)*HP(LIJ(I,J))*DXU(LIJ(I,J))*SED(LIJ(I,J),1,K)
                     tss_flux_u(LOC)=tss_flux_u(LOC)+tss_flux_u_tmp
                     tss_flux_v_tmp=V(LIJ(I,J),1)*HP(LIJ(I,J))*DYV(LIJ(I,J))*SED(LIJ(I,J),1,K)
@@ -104,19 +119,20 @@ DO  L=2,LA
     zeta(L)=(HP(L)+BELV(L))
 ENDDO 
 
-! Write velocity calibration data each call
-WRITE(112,'(16F8.4)') time_efdc,zeta(LIJ(122,114)), &
+! Write velocity calibration file
+WRITE(112,298) time_efdc,zeta(LIJ(122,114)), &
     U(LIJ(122,114),1),V(LIJ(122,114),1),zeta(LIJ(45,29)),U(LIJ(45,29),1),V(LIJ(45,29),1), &
     zeta(LIJ(39,202)),U(LIJ(39,202),1),V(LIJ(39,202),1),zeta(LIJ(119,312)),U(LIJ(119,312),1), &
     V(LIJ(119,312),1),zeta(LIJ(130,349)),U(LIJ(130,349),1),V(LIJ(130,349),1)
     
     ! Write flow rate file
-    WRITE(109,'(9F10.4)')  time_efdc, flow_u(1),flow_v(1), flow_u(2),flow_v(2), &
-    flow_u(3),flow_v(3), flow_u(4),flow_v(4)
+    WRITE(109,299)  time_efdc, flow_u(1),flow_v(1), flow_u(2),flow_v(2), &
+    flow_u(3),flow_v(3), flow_u(4),flow_v(4), flow_u(5),flow_v(5), &
+	flow_u(6),flow_v(6)
 
 IF (ISTRAN(1).EQ.1.OR.ISTRAN(3).EQ.1) THEN
-    ! Write tracer calibration data each call
-    WRITE(115,'(11F8.4)') time_efdc,SAL(LIJ(122,114),1), &
+    ! Write tracer calibration file
+    WRITE(115,300) time_efdc,SAL(LIJ(122,114),1), &
     DYE(LIJ(122,114),1),SAL(LIJ(45,29),1),DYE(LIJ(45,29),1),SAL(LIJ(39,202),1),DYE(LIJ(39,202),1), &
     SAL(LIJ(119,312),1),DYE(LIJ(119,312),1),SAL(LIJ(130,349),1),DYE(LIJ(130,349),1)
 ENDIF
@@ -125,44 +141,52 @@ ENDIF
 IF(ISTRAN(6).EQ.1) THEN
     ! TSS calibration file with SEDZLJ (hard coded for 1 water layer (KC))
     DO K=1,NSCM
-        WRITE(105,299)  time_efdc, K, SED(LIJ(122,114),1,K), &
+        WRITE(105,301)  time_efdc, K, SED(LIJ(122,114),1,K), &
         SED(LIJ(45,29),1,K), SED(LIJ(39,202),1,K), SED(LIJ(119,312),1,K), SED(LIJ(130,349),1,K)
     ENDDO
 
-    WRITE(106,'(6F8.4)')  time_efdc, THCK(LIJ(122,114)), THCK(LIJ(45,29)), &
+    ! Write thickness file for MHS01,MHS02,MHS05,MHS06, and MHS07
+    WRITE(106,302)  time_efdc, THCK(LIJ(122,114)), THCK(LIJ(45,29)), &
     THCK(LIJ(39,202)), THCK(LIJ(119,312)), THCK(LIJ(130,349))
 
     ! Write TSS flux file if SED is activated
-    WRITE(108,'(9F12.4)')  time_efdc, tss_flux_u(1),tss_flux_v(1), tss_flux_u(2),tss_flux_v(2), &
-    tss_flux_u(3),tss_flux_v(3), tss_flux_u(4),tss_flux_v(4)
+    WRITE(108,303)  time_efdc, tss_flux_u(1),tss_flux_v(1), tss_flux_u(2),tss_flux_v(2), &
+    tss_flux_u(3),tss_flux_v(3), tss_flux_u(4),tss_flux_v(4), tss_flux_u(5),tss_flux_v(5), &
+	tss_flux_u(6),tss_flux_v(6)
+	
 ENDIF
 
-DO J=3,JC-2
-    DO I=3,IC-2
-        IF(LIJ(I,J)>0) THEN
-            L=LIJ(I,J)
-            IF(LMASKDRY(L).AND.HP(L).GT.0.3) THEN
-
-                IF(vel_maxc(L).GT.vel_max(L)) THEN
-                    vel_max(L)=vel_maxc(L)
-                ENDIF
-
-                IF(TAU(L).GT.tau_max(L)) THEN
-                    tau_max(L)=TAU(L)
-                ENDIF
-
-            ENDIF
-        ENDIF
-    ENDDO
-ENDDO
+!DO J=3,JC-2
+!    DO I=3,IC-2
+!        IF(LIJ(I,J)>0) THEN
+!            L=LIJ(I,J)
+!            IF(LMASKDRY(L).AND.HP(L).GT.0.3) THEN
+!
+!                IF(vel_maxc(L).GT.vel_max(L)) THEN
+!                    vel_max(L)=vel_maxc(L)
+!                ENDIF
+!
+!                IF(TAU(L).GT.tau_max(L)) THEN
+!                    tau_max(L)=TAU(L)
+!                ENDIF
+!
+!            ENDIF
+!        ENDIF
+!    ENDDO
+!ENDDO
 
 ! Shear stress calibration file
-WRITE(107,'(11F10.4)') time_efdc,TAU(LIJ(122,114)),tau_max(LIJ(122,114)),TAU(LIJ(45,29)),tau_max(LIJ(45,29)), &
-    TAU(LIJ(39,202)),tau_max(LIJ(39,202)),TAU(LIJ(119,312)),tau_max(LIJ(119,312)), &
-    TAU(LIJ(130,349)),tau_max(LIJ(130,349))
-
-! Format for tss_cal.dat file
-299 FORMAT(F7.4,2X,I1,F12.3,F12.3,F12.3,F12.3,F12.3)
+WRITE(107,304) time_efdc,TAU(LIJ(122,114)),TAU(LIJ(45,29)), &
+    TAU(LIJ(39,202)),TAU(LIJ(119,312)),TAU(LIJ(130,349))
+	
+! Write formats 
+298 FORMAT(F10.6,15F9.3)
+299 FORMAT(F10.6,12F10.3)
+300 FORMAT(F10.6,10F9.3)
+301 FORMAT(F10.6,2X,I1,5F12.3)
+302 FORMAT(F10.6,5F9.3)
+303 FORMAT(F10.6,12F15.3)
+304 FORMAT(F10.6,5F8.3)
 
 FLUSH(112)
 FLUSH(109)
